8e6d5160cde211c30d55068f0d7eb3490dd225c2
Jeffrey Byrd <jeffreydbyrd@gmail.com>
2012-10-01 22:00:43 -0400

    last git pull screwed up formatting

--- Event/EventHandler.py
+++ Event/EventHandler.py
@@ -41,25 +41,4 @@ class EventHandler:
 				break
 		
 		if event.attributes['signature'] != None:
-<<<<<<< HEAD
 			self.handleEvent(event)
-			
-			
-			
-	def loadAdjusterFromJSON(self, adjusterJSON):
-		adjusterName	= adjusterJSON['name']
-		args			= (lambda dictionary : dictionary.has_key('args') and dictionary['args'] or None)(adjusterJSON)
-		path			= adjusterName.split('.')
-		modulePath		= path[0]
-
-		for step in path[1:-1]:
-			modulePath = '{}.{}'.format(modulePath, step)
-
-		adjusterModule	= importlib.import_module(modulePath)
-		adjusterClass	= getattr(adjusterModule, path[-1])
-		adjuster		= adjusterClass(args)
-		
-		return adjuster
-=======
-			self.handleEvent(event)
->>>>>>> bf64e254f8059325d6762aa2de1dc819677452d0
